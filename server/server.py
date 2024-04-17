1f52fdb62227887e68a579cae73249a52e5fd229
Arooj_Pandey <aroojpandey@bbdnitm.ac.in>
2024-04-17 15:18:32 +0530

    C:projectsSportsPersonClassifier.idea.gitignore

--- server/server.py
+++ server/server.py
@@ -1,22 +1,10 @@
 from flask import Flask, request, jsonify
-import util
-
 
 app = Flask(__name__)
 
-@app.route('/classify_image', methods = ['GET', 'POST'])
-
+@app.route('/classify_image', methods = ['Get', 'POST'])
 def classify_image():
-    image_data = request.form['image_data']
-    response = jsonify(util.classify_image(image_data))
-
-    response.headers.add('Access-Control-Allow-Origin','*')
-
-    return response
-
-
+    return "hi"
 
 if __name__ == "__main__":
-    print('Starting Python Flask Server for Sports Celebrity Image Classification')
-    util.load_saved_artifacts()
     app.run(port=5000)
